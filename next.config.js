f066b09e7f7bcb8b4235528d3ff0052594280bf3
rootEnginear <rootEnginear@users.noreply.github.com>
2023-03-19 04:06:47 +0700

    fix: add prod `basePath`, add `.nojekyll`

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/tpc-course-planner" : "",
 };
 
 module.exports = nextConfig;
