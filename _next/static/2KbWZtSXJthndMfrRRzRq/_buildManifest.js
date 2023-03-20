8b4cc5815e9f3c052b5912127cdec5c5b205b404
rootEnginear <suthep.chanchuphol@outlook.com>
2023-03-20 02:53:03 +0000

    Update gh-pages to output generated at 02f7244

--- _next/static/2KbWZtSXJthndMfrRRzRq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3e2604d16f2a9f1d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
