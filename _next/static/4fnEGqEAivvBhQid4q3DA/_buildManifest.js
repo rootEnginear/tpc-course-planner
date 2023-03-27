804c2ccd5a912ef77b32a4f43652f82d121be0a9
rootEnginear <suthep.chanchuphol@outlook.com>
2023-03-27 22:55:38 +0000

    Update gh-pages to output generated at ae5c76b

--- _next/static/4fnEGqEAivvBhQid4q3DA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/917-41f9dfdec2ac42cb.js","static/chunks/pages/index-d4560e8b7d8b2991.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/WGiMWl4lU6mrC0SzEXxVO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/917-41f9dfdec2ac42cb.js","static/chunks/pages/index-ca4d3251c602bddf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
